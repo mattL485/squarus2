bd9f573cecd3434c32f6add4df029fc8fc265095
Matt <mattL485>
2021-03-26 18:56:22 -0400

    added rudimentary turn based system and quit functionality.
    Signed-off-by: Matt <mattL485>

--- board.py
+++ board.py
@@ -23,21 +23,36 @@ for row in range(maxSquares):
         board_rect[len(board) - 1].x = col * 50
         board_rect[len(board) - 1].y = row * 50
 
-
 font = pygame.font.Font('freesansbold.ttf', 32)
-text = font.render('Quit', True, (0,0,255), (0,255,0))
-textRect = text.get_rect()
-textRect.center = (1400, 100)
+quit_text = font.render('Quit', True, (0, 0, 255), (0, 255, 0))
+quit_rect = quit_text.get_rect()
+quit_rect.center = (1400, 100)
+
+set_text = font.render('Confirm Placement', True, (0, 0, 255), (0, 255, 0))
+set_rect = set_text.get_rect()
+set_rect.center = (1400, 800)
 
 # initialize pieces:
 pieces = list()
 pieces.append(piece((1200, 200), 0, [(1000, 200, 50, 50), (1000, 250, 50, 50), (1050, 250, 50, 50)]))
-pieces.append(piece((1200, 200), 0, [(1200, 200, 50, 50), (1200, 250, 50, 50), (1200, 300, 50, 50), (1250, 300, 50, 50)]))
-pieces.append(piece((1400, 200), 1, [(1400, 200, 50, 50), (1400, 250, 50, 50), (1400, 300, 50, 50), (1450, 300, 50, 50)]))
-rectDist = math.dist([0, 0], [25, 25])
+pieces.append(
+    piece((1200, 200), 0, [(1200, 200, 50, 50), (1200, 250, 50, 50), (1200, 300, 50, 50), (1250, 300, 50, 50)]))
+pieces.append(
+    piece((1400, 200), 1, [(1400, 200, 50, 50), (1400, 250, 50, 50), (1400, 300, 50, 50), (1450, 300, 50, 50)]))
+# rectDist = math.dist([0, 0], [25, 25])
+rectDist = 20
 
 # set Team to 0
 current_team = 0
+
+# the last touched piece:
+last_piece = pieces[0]
+
+
+def checkValid(placementPiece, pieces):
+    print("checking validity of pieces.")
+    return True
+
 
 # main event loop
 while 1:
@@ -49,15 +64,26 @@ while 1:
         # mouse press event
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
+                mouse_x, mouse_y = event.pos
                 for piece in pieces:
+                    if piece.set:
+                        continue
                     for rectIt in range(len(piece.rects)):
                         # collision check:
                         if piece.rects[rectIt][0].collidepoint(event.pos) and piece.team == current_team:
                             piece.drag = True
-                            mouse_x, mouse_y = event.pos
                             for rectIt in range(len(piece.rects)):
                                 piece.rects[rectIt][1][0] = mouse_x
                                 piece.rects[rectIt][1][1] = mouse_y
+                if math.dist([mouse_x, mouse_y], [quit_rect.centerx, quit_rect.centery]) < 50:
+                    sys.exit()
+                elif math.dist([mouse_x, mouse_y], [set_rect.centerx, set_rect.centery]) < 50:
+                    last_piece.set = True
+                    if current_team == 0:
+                        current_team = 1
+                    elif current_team == 1:
+                        current_team = 0
+                    print("piece set!")
 
         # mouse release event
         elif event.type == pygame.MOUSEBUTTONUP:
@@ -65,6 +91,8 @@ while 1:
                 # the board iterator, iterates through the rect pieces.
                 for boardIt in range(len(board_rect)):
                     for piece in pieces:
+                        if piece.set:
+                            continue
                         piece.drag = False
                         for rectIt in range(len(piece.rects)):
                             # TODO: bounds checking for placement
@@ -72,12 +100,20 @@ while 1:
                             board_piece = [board_rect[boardIt].centerx, board_rect[boardIt].centery]
                             placement_dist = math.dist(center_piece, board_piece)
                             if placement_dist <= rectDist:
-                                piece.square_list.append(boardIt)
-                                piece.rects[piece.square_count][0].x = board_rect[boardIt].x
-                                piece.rects[piece.square_count][0].y = board_rect[boardIt].y
+                                piece.square_list.append([piece.square_count, boardIt])
+                                # piece.rects[piece.square_count][0].x = board_rect[boardIt].x
+                                # piece.rects[piece.square_count][0].y = board_rect[boardIt].y
                                 piece.square_count += 1
                                 if piece.square_count == len(piece.rects):
                                     piece.square_count = 0
+                                    if checkValid(piece, pieces):
+                                        for pieceIt in range(len(piece.rects)):
+                                            piece.rects[piece.square_list[pieceIt][0]][0].x = board_rect[
+                                                piece.square_list[pieceIt][1]].x
+                                            piece.rects[piece.square_list[pieceIt][0]][0].y = board_rect[
+                                                piece.square_list[pieceIt][1]].y
+                                        piece.square_list.clear()
+                                        last_piece = piece
 
 
         # mouse movement
@@ -91,7 +127,6 @@ while 1:
                         piece.rects[rectIt][1][0] = mouse_x
                         piece.rects[rectIt][1][1] = mouse_y
 
-
     screen.fill((40, 40, 40))
     for boardIt in range(len(board)):
         screen.blit(board[boardIt], board_rect[boardIt])
@@ -101,5 +136,6 @@ while 1:
                 pygame.draw.rect(screen, (255, 0, 0), piece.rects[rectIt][0])
             else:
                 pygame.draw.rect(screen, (0, 255, 0), piece.rects[rectIt][0])
-    screen.blit(text, textRect)
+    screen.blit(quit_text, quit_rect)
+    screen.blit(set_text, set_rect)
     pygame.display.flip()
--- pieces.py
+++ pieces.py
@@ -2,11 +2,13 @@ import pygame
 
 class piece:
     drag = False
+    set = False
     square_count = 0
     square_list = list()
     def __init__(self, pos, team, rects):
         self.pos = pos
         self.team = team
         self.rects = list()
+        self.start_rects = list()
         for rectIt in range(len(rects)):
             self.rects.append([pygame.rect.Rect(rects[rectIt]), [0, 0]])
