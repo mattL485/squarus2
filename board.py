519346d1fe56e0fcfc72e509e321dba13694e1f1
Matt <mattL485>
2021-07-30 12:31:35 -0400

    added piece snapping function and additional comments.
    Signed-off-by: Matt <mattL485>

--- board.py
+++ board.py
@@ -49,9 +49,25 @@ current_team = 0
 last_piece = pieces[0]
 
 
-def checkValid(placementPiece, pieces):
+def check_valid(placementPiece, pieces):
     print("checking validity of pieces.")
+    # checks for: overlap, corner, and side touching of adjacent pieces:
+
+    print("the location of the piece is:")
+    print(placementPiece.square_list)
     return True
+
+
+def snap_pieces(piece, board_rect):
+    for pieceIt in range(len(piece.rects)):
+        # iterates through the pieces and
+        piece.rects[piece.square_list[pieceIt][0]][0].x = board_rect[
+            piece.square_list[pieceIt][1]].x
+        piece.rects[piece.square_list[pieceIt][0]][0].y = board_rect[
+            piece.square_list[pieceIt][1]].y
+    piece.square_list.clear()
+    last_piece = piece  # it may not be necessary to keep track of the last placed piece.
+    return 0
 
 
 # main event loop
@@ -61,11 +77,12 @@ while 1:
         # QUIT event
         if event.type == pygame.QUIT:
             sys.exit()
-        # mouse press event
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
+                # mouse press event
                 mouse_x, mouse_y = event.pos
                 for piece in pieces:
+                    # iterate through the piece list
                     if piece.set:
                         continue
                     for rectIt in range(len(piece.rects)):
@@ -87,14 +104,19 @@ while 1:
 
         # mouse release event
         elif event.type == pygame.MOUSEBUTTONUP:
+            # left mouse button release
             if event.button == 1:
                 # the board iterator, iterates through the rect pieces.
                 for boardIt in range(len(board_rect)):
+                    # iterate through the entire board
                     for piece in pieces:
+                        # iterate through the piece list
                         if piece.set:
+                            # if a piece has been set, ignore it.
                             continue
                         piece.drag = False
                         for rectIt in range(len(piece.rects)):
+                            # iterates through the rectangles (squares) in the rectangle list for that piece
                             # TODO: bounds checking for placement
                             center_piece = [piece.rects[rectIt][0].centerx, piece.rects[rectIt][0].centery]
                             board_piece = [board_rect[boardIt].centerx, board_rect[boardIt].centery]
@@ -104,16 +126,14 @@ while 1:
                                 # piece.rects[piece.square_count][0].x = board_rect[boardIt].x
                                 # piece.rects[piece.square_count][0].y = board_rect[boardIt].y
                                 piece.square_count += 1
-                                if piece.square_count == len(piece.rects):
-                                    piece.square_count = 0
-                                    if checkValid(piece, pieces):
-                                        for pieceIt in range(len(piece.rects)):
-                                            piece.rects[piece.square_list[pieceIt][0]][0].x = board_rect[
-                                                piece.square_list[pieceIt][1]].x
-                                            piece.rects[piece.square_list[pieceIt][0]][0].y = board_rect[
-                                                piece.square_list[pieceIt][1]].y
-                                        piece.square_list.clear()
-                                        last_piece = piece
+                        if piece.square_count == len(piece.rects):
+                            # this indicates that all of the pieces have a correlatory board space.
+                            print("the square count equals the rectangles")
+                            piece.square_count = 0
+                            snap_pieces(piece, board_rect)
+                            if check_valid(piece, pieces):
+                                piece.set = True
+
 
 
         # mouse movement
--- pieces.py
+++ pieces.py
@@ -6,9 +6,9 @@ class piece:
     square_count = 0
     square_list = list()
     def __init__(self, pos, team, rects):
-        self.pos = pos
-        self.team = team
-        self.rects = list()
-        self.start_rects = list()
+        self.pos = pos  # this is the location of the piece in reference to the first piece in the rects list
+        self.team = team  # this is the integer that indicates the piece's team.
+        self.rects = list()  # this is the list of rectangles that are placed on the board
+        self.start_rects = list()  # this is an un-utilized list
         for rectIt in range(len(rects)):
             self.rects.append([pygame.rect.Rect(rects[rectIt]), [0, 0]])
